60a1925a82d312f482af1fdb4dedd53857b9e9a8
zkdn46 <123gusckd123@naver.com>
2024-11-11 17:04:25 +0900

    Add Unit Random()

--- zombie/src/zombie/Unit.java
+++ zombie/src/zombie/Unit.java
@@ -1,9 +1,13 @@
 package zombie;
 
+import java.util.Random;
+
 abstract class Unit {
+	protected Random ran = new Random();
+	
 	protected int pos;
 	protected int hp;
-	protected int damage;
+	protected int maxDamage;
 
 	protected abstract void attack(Unit enenmy);
 }

2ce7f12d19101d288508ec76bbe1e156ebfaf403
zkdn46 <123gusckd123@naver.com>
2024-11-11 17:16:14 +0900

    Add Play set()

--- zombie/src/zombie/Play.java
+++ zombie/src/zombie/Play.java
@@ -1,5 +1,29 @@
 package zombie;
 
-public class Play {
+import java.util.Scanner;
 
+public class Play {
+	protected Scanner sc = new Scanner(System.in);
+	private Hero hero;
+	private Boss boss;
+	private Zombie zombie;
+	private boolean isRun = true;
+
+	public void run() {
+		set();
+		while (isRun) {
+
+			end();
+		}
+	}
+
+	private void set() {
+		zombie = new Zombie(5, 10, 100);
+		boss = new Boss(9, 20, 300, 100);
+		hero = new Hero(0, 20, 200, 2);
+	}
+
+	private void end() {
+
+	}
 }

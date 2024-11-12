6aa9f84aa1c7bf19e79644d464d1101cbb920652
zkdn46 <123gusckd123@naver.com>
2024-11-12 12:16:29 +0900

    Fix Boss String msg

--- zombie/src/zombie/Boss.java
+++ zombie/src/zombie/Boss.java
@@ -18,18 +18,17 @@ public class Boss extends Unit {
 		int critical = ran.nextInt(4);
 		if (critical == DOUBLE) {
 			hero.hp -= damage * DOUBLE;
-			String msg = String.format("필살기! Hero에게 %d의 치명타 피해 입힘, 현재 Hero 체력: %d", damage * DOUBLE, hero.hp);
-			System.out.println(msg);
+			String msg = String.format("필살기! Hero에게 %d의 치명타 피해 입힘, ", damage * DOUBLE);
+			System.out.print(msg);
 		} else {
 			hero.hp -= damage;
-			String msg = String.format("Hero에게 %d의 피해 입힘, 현재 Hero 체력: %d", damage, hero.hp);
-			System.out.println(msg);
+			String msg = String.format("Hero에게 %d의 피해 입힘, ", damage);
+			System.out.print(msg);
 		}
-
 		if (hero.hp < EMPTY) {
 			hero.hp = EMPTY;
 		}
-
+		String msg = String.format("현재 Hero 체력: %d", hero.hp);
+		System.out.println(msg);
 	}
-
 }

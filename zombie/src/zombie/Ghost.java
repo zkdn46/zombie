314ff8e321e50882a48f61b38715ecb49c6254f1
zkdn46 <123gusckd123@naver.com>
2024-11-12 12:39:07 +0900

    Create Class Ghost

---
+++ zombie/src/zombie/Ghost.java
@@ -0,0 +1,10 @@
+package zombie;
+
+public class Ghost extends Zombie {
+
+	protected Ghost(int pos, int maxDamage, int hp) {
+		super(pos, maxDamage, hp);
+		// TODO Auto-generated constructor stub
+	}
+	
+}

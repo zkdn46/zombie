d082cb87a98f204e5b9414b7bbd5ba9e5b7d5a15
zkdn46 <123gusckd123@naver.com>
2024-11-12 12:45:40 +0900

    Add Ghost attack()

--- zombie/src/zombie/Ghost.java
+++ zombie/src/zombie/Ghost.java
@@ -4,7 +4,17 @@ public class Ghost extends Zombie {
 
 	protected Ghost(int pos, int maxDamage, int hp) {
 		super(pos, maxDamage, hp);
-		// TODO Auto-generated constructor stub
+	}
+	
+	protected void attack(Unit hero) {
+		int damage = ran.nextInt(maxDamage) + 1;
+		hero.hp -= damage;
+		if (hero.hp < 0) {
+			hero.hp = 0;
+		}
+
+		String msg = String.format("유령이 %d의 피해를 입힘, 현재 Hero체력: %d", damage, hero.hp);
+		System.out.println(msg);
 	}
 	
 }

284f3bf3aa82d2fa0e4b4f03711619ed80088ce0
zkdn46 <123gusckd123@naver.com>
2024-11-12 11:36:25 +0900

    Fix Hero enemy.hp

--- zombie/src/zombie/Hero.java
+++ zombie/src/zombie/Hero.java
@@ -10,28 +10,31 @@ public class Hero extends Unit {
 		this.potion = potion;
 	}
 
-	protected void attack(Unit enenmy) {
+	protected void attack(Unit enemy) {
 		int damage = ran.nextInt(maxDamage) + 1;
 
-		if (enenmy instanceof Boss) {
-			if (((Boss) enenmy).shield != EMPTY) {
-				((Boss) enenmy).shield -= damage;
-				if (((Boss) enenmy).shield <= EMPTY) {
-					((Boss) enenmy).shield = EMPTY;
+		if (enemy instanceof Boss) {
+			if (((Boss) enemy).shield != EMPTY) {
+				((Boss) enemy).shield -= damage;
+				if (((Boss) enemy).shield <= EMPTY) {
+					((Boss) enemy).shield = EMPTY;
 					System.out.println("보스 실드 파괴!");
 				}
 			} else {
-				enenmy.hp -= damage;
-				if (enenmy.hp < EMPTY) {
-					enenmy.hp = EMPTY;
+				enemy.hp -= damage;
+				if (enemy.hp < EMPTY) {
+					enemy.hp = EMPTY;
 				}
 			}
-			String msg = String.format("보스에게 %d의 피해 입힘, 현재 보스 체력: %d / 실드: %d", damage, enenmy.hp,
-					((Boss) enenmy).shield);
+			String msg = String.format("보스에게 %d의 피해 입힘, 현재 보스 체력: %d / 실드: %d", damage, enemy.hp,
+					((Boss) enemy).shield);
 			System.out.println(msg);
 		} else {
-			enenmy.hp -= damage;
-			String msg = String.format("좀비에게 %d의 피해 입힘, 현재 좀비 체력: %d", damage, enenmy.hp);
+			enemy.hp -= damage;
+			if (enemy.hp < EMPTY) {
+				enemy.hp = EMPTY;
+			}
+			String msg = String.format("좀비에게 %d의 피해 입힘, 현재 좀비 체력: %d", damage, enemy.hp);
 			System.out.println(msg);
 		}
 

7dd5a72641cdea7aa7847468d4372321d667129e
zkdn46 <123gusckd123@naver.com>
2024-11-12 13:52:32 +0900

    Create class Mage

---
+++ zombie/src/zombie/Mage.java
@@ -0,0 +1,10 @@
+package zombie;
+
+public class Mage extends Zombie {
+
+	protected Mage(int pos, int maxDamage, int hp) {
+		super(pos, maxDamage, hp);
+		// TODO Auto-generated constructor stub
+	}
+
+}
